dfba0708c63c2e93d4d1b3e4985d3aee9c96a3db
小朋 <2810580330@qq.com>
2020-10-05 20:21:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/share/v2ray
+  #!/bin/bash
+# 开启转发
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
 echo -e "nameserver 114.114.114.114" > /etc/resolv.conf
