0ecffad03843ead1b3fcf5c522a4736986b23f5c
小朋 <2810580330@qq.com>
2020-10-05 20:13:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/share/v2ray
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
+echo -e "nameserver 114.114.114.114" > /etc/resolv.conf
 v2raya --mode=universal --webdir=/etc/v2raya-web
