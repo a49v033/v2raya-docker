daf6482cfed1bef6623889bb9a9d6c8fa31874e6
小朋 <2810580330@qq.com>
2020-10-09 22:02:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 # 开启转发
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
-echo -e "nameserver 114.114.114.114" > /etc/resolv.conf
+echo -e "nameserver 180.76.76.76" > /etc/resolv.conf
 v2raya --mode=universal --webdir=/etc/v2raya-web
