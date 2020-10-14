3cc6a7e16d011500cd81c237eb3afbcd6ed76916
小朋 <2810580330@qq.com>
2020-10-14 23:59:14 +0800

    AdGuardHome

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
 #echo -e "nameserver 180.76.76.76" > /etc/resolv.conf
+AdGuardHome -c /etc/v2raya/AdGuardHome.yaml >/dev/null 2>&1 &
 v2raya --mode=universal --webdir=/etc/v2raya-web
