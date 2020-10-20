2b2524fb42b16568e501ee788e7695f22d0d75ed
小朋 <2810580330@qq.com>
2020-10-20 18:55:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 开启转发
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
+echo "net.ipv4.tcp_fastopen=3" >> /etc/sysctl.conf
 sysctl -p
 #echo -e "nameserver 180.76.76.76" > /etc/resolv.conf
 #AdGuardHome -c /etc/v2raya/AdGuardHome.yaml >/dev/null 2>&1 &
