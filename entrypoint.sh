44ae8550206a0e006844b7c9c568a5d8c9f47ef3
小朋 <2810580330@qq.com>
2020-10-20 22:51:17 +0800

    删除AdGuardHome

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# 开启转发
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
 #echo -e "nameserver 180.76.76.76" > /etc/resolv.conf
-#AdGuardHome -c /etc/v2raya/AdGuardHome.yaml >/dev/null 2>&1 &
 v2raya --mode=universal --webdir=/etc/v2raya-web
