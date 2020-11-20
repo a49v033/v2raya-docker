5f4de1d3a90a5b51c4c397bb866c5d02630c95eb
小朋 <2810580330@qq.com>
2020-11-20 08:42:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 #sysctl -p
-#由于国内dns会解析github到无法访问ip  添加hosts
-echo '192.30.255.113 github.com' >> /etc/hosts 
+echo -e "nameserver 180.76.76.76" > /etc/resolv.conf 
 v2raya --mode=universal --webdir=/etc/v2raya-web
