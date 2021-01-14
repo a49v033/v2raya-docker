ba73dfeeff997ff6b572ad1c2664eeea68278563
小朋 <2810580330@qq.com>
2021-01-14 17:17:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 #sysctl -p
-sdns -config=/etc/v2raya/sdns.conf >/dev/null 2>&1 &
+#sdns -config=/etc/v2raya/sdns.conf >/dev/null 2>&1 &
+smartdns -c /etc/v2raya/smartdns.conf
 v2raya --mode=universal --webdir=/etc/v2raya-web
