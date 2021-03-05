c2ec1bf1c8040d9fa5dce85fb9a5a6cc59dd0411
小朋 <2810580330@qq.com>
2021-03-06 00:27:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #sdns -config=/etc/v2raya/sdns.conf >/dev/null 2>&1 &
-smartdns -c /etc/v2raya/smartdns.conf
+#smartdns -c /etc/v2raya/smartdns.conf
 v2raya --mode=universal --webdir=/etc/v2raya-web
