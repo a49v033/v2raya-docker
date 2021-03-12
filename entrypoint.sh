1d0dcc2aa8a3c3c437149caf1e72c66b4bcc1f81
小朋 <2810580330@qq.com>
2021-03-12 22:43:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -e '/usr/local/share/v2ray/geoip.dat' ]; then
+if [ ! -e '/usr/local/share/v2ray/LoyalsoldierSite.dat' ]; then
     cp /tmp/v2ray/LoyalsoldierSite.dat /usr/local/share/v2ray/LoyalsoldierSite.dat
     cp /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
     cp /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
