ef009957ebd6863a4b72aa1f15b905b9216e8f9e
小朋 <2810580330@qq.com>
2021-04-12 15:55:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ if [ ! -e '/usr/local/share/v2ray/LoyalsoldierSite.dat' ]; then
     cp /tmp/v2ray/LoyalsoldierSite.dat /usr/local/share/v2ray/LoyalsoldierSite.dat
     cp /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
     cp /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
+    rm -rf /tmp/v2ray
     echo "拷贝geoip文件"
 fi
 v2raya --mode=universal --webdir=/etc/v2raya-web
